6bd0900c67aef722b5ce52f3bd753ca81bd93def
DatDonathan <jojo08082003@gmail.com>
2021-12-21 11:49:58 +0100

    Added game engine

---
+++ game-engine/util.js
@@ -0,0 +1,14 @@
+export class Vector2D {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+export class Vector3D {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
